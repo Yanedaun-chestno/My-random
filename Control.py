b524d60baa7c5ba751bc485a8b18777f773f737c
Yanedaun-chestno <tanag2447@gmail.com>
2025-12-28 15:30:07 +0300

    сообщение

---
+++ Control.py
@@ -0,0 +1,20 @@
+import MyRandomTeto
+
+print(
+'''Привет!
+Это генератор рандомных чисел
+Введите:
+0 - получить число из вашего диапазона
+1 - получить x чисел из вашего диапазона'''
+)
+p = input()
+
+if p == 0:
+    #print(myrandom())
+    
+elif p == 1:
+        z = int(input())
+    for z in range():
+        print(myrandom())
+else:
+    
